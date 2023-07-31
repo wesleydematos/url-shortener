f263b49622379d5a83504040aa5c53ab3b1530cf
wesleydematos <wesleydematos3@gmail.com>
2023-07-31 13:50:47 -0300

    test: implemented new test

--- src/test/java/com/shortener/urlshortner/UrlShortnerApplicationTests.java
+++ src/test/java/com/shortener/urlshortner/UrlShortnerApplicationTests.java
@@ -1,10 +1,11 @@
 package com.shortener.urlshortner;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -26,5 +27,21 @@ class UrlShortnerApplicationTests {
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
 
+    @Test
+    public void testRetrieveUrls() throws Exception {
+        String originalUrl = "{\"full_url\": \"http://www.example.com/long-url\"}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/shortenurl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(originalUrl))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/shortenurl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(originalUrl))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/urls")).andExpect(MockMvcResultMatchers.status().isOk());
+    }
 
 }
